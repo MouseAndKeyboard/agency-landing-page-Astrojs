32cbc59a292775faeb537679056977095d466a4e
cjnbennett <conor.jn.bennett@gmail.com>
2025-04-12 19:19:04 +0800

    defense -> defence

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -16,9 +16,9 @@ const services = [
              </svg>`,
     },
     {
-      title: "Defense & GovTech Projects",
+      title: "Defence & GovTech Projects",
       description:
-        "Provide specialised rapid development for defense and government agencies, delivering secure, mission-critical solutions at unprecedented speed.",
+        "Provide specialised rapid development for defence and government agencies, delivering secure, mission-critical solutions at unprecedented speed.",
       icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
              </svg>`,
